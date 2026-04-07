d92f9c2f609646d870d4de98a33e5564a54a9cd8
serhattie <serhat.ali.camur@gmail.com>
2026-04-07 13:16:44 +0200

    start soundmanager

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioSource soundUI;
+    public AudioSource walkWheel;
+    public AudioSource walkBlind;
+    public AudioSource musicInGame;
+    public AudioSource 
+    public AudioSource
+    public AudioSource
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
