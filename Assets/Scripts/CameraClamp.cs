b2c6853c00fe4d1777f52cd31153484543e89c14
sigurdpade <sigurdpaden@gmail.com>
2026-04-24 13:45:17 +0200

    We hate cameras

--- Assets/Scripts/CameraClamp.cs
+++ Assets/Scripts/CameraClamp.cs
@@ -7,29 +7,24 @@ public class CameraClamp : MonoBehaviour
     [SerializeField] Tilemap worldTileMap;
     [SerializeField] Transform player;
 
-    [SerializeField] float minOffSetX;
-    [SerializeField] float minOffSetY;
-    [SerializeField] float maxOffSetX;
-    [SerializeField] float maxOffSetY;
-
     void Update()
     {
+        // Compress bounds to only include existing tiles
+        worldTileMap.CompressBounds();
+
         //Get the boundaries of the tilemap
         Bounds cameraBounds = worldTileMap.localBounds;
-
-        //Attach camera to player position
         Vector3 targetPos = new Vector3(player.position.x, player.position.y, playerCam.transform.position.z);
 
-        //Define the camera dimensions
+        // Define the camera dimensions
         float halfHeight = playerCam.orthographicSize;
         float halfWidth = halfHeight * playerCam.aspect;
 
-        //Clamp the target position to stay within bounds
-        targetPos.x = Mathf.Clamp(targetPos.x, cameraBounds.min.x + minOffSetX + halfWidth, cameraBounds.max.x - maxOffSetX - halfWidth);
-        targetPos.y = Mathf.Clamp(targetPos.y, cameraBounds.min.y + minOffSetY + halfHeight, cameraBounds.max.y - maxOffSetY - halfHeight);
-        Debug.Log("Camera Position: " + targetPos);
-
-        //Apply the target position
+        // Clamp the target position to stay within bounds
+        targetPos.x = Mathf.Clamp(targetPos.x, cameraBounds.min.x + halfWidth, cameraBounds.max.x - halfWidth);
+        targetPos.y = Mathf.Clamp(targetPos.y, cameraBounds.min.y + halfHeight, cameraBounds.max.y - halfHeight);
+        Debug.Log($"Camera Bounds: {cameraBounds.min.x}, {cameraBounds.min.y} to {cameraBounds.max.x}, {cameraBounds.max.y}");
+        // Apply the target position
         playerCam.transform.position = targetPos;
     }
 }
