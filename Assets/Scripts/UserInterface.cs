0efa6b100cba47d3cef6f86ee4bceb77c237ec99
serhattie <serhat.ali.camur@gmail.com>
2026-05-05 12:54:45 +0200

    Main Menu yayayayyaya

--- Assets/Scripts/UserInterface.cs
+++ Assets/Scripts/UserInterface.cs
@@ -5,14 +5,33 @@ using UnityEngine.SceneManagement;
 public class UserInterface : MonoBehaviour
 {
     public GameObject pauseUI;
+    public GameObject levelSelectUI;
+    public GameObject MainMenuUI;
     private bool isPaused = false;
 
-    void Update()
-    {
-        if (pauseUI == null) return;
-        if (Keyboard.current == null) return;
+    // PAUSE
 
-        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+    private void Start()
+    {
+        if (levelSelectUI != null)
+            levelSelectUI.SetActive(false);
+    }
+
+    void Update()
+{
+    if (Keyboard.current == null) return;
+
+    if (Keyboard.current.escapeKey.wasPressedThisFrame)
+    {
+        if (levelSelectUI != null && levelSelectUI.activeSelf)
+
+        {
+            CloseLevelSelect();
+            BackToMain();
+            return;
+        }
+
+        if (pauseUI != null)
         {
             if (isPaused)
                 Resume();
@@ -20,6 +39,7 @@ public class UserInterface : MonoBehaviour
                 Pause();
         }
     }
+}
 
     public void Pause()
     {
@@ -38,8 +58,8 @@ public class UserInterface : MonoBehaviour
         Time.timeScale = 1f;
         isPaused = false;
     }
-    
-    public void RestartLevel()  
+
+    public void RestartLevel()
     {
         Time.timeScale = 1f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
@@ -48,18 +68,40 @@ public class UserInterface : MonoBehaviour
     public void MainMenu()
     {
         Time.timeScale = 1f;
+        isPaused = false;
         SceneManager.LoadScene("MainMenu");
     }
 
-    // SCENE
+    // MENU
 
     public void StartGame()
     {
         Time.timeScale = 1f;
-        SceneManager.LoadScene("Level1");
+        SceneManager.LoadScene("SERHAT TEST");
     }
 
-    public void QuitGame()
+    public void OpenLevelSelect()
+    {
+        if (levelSelectUI != null)
+            levelSelectUI.SetActive(true);
+
+        if (MainMenuUI != null)
+            MainMenuUI.SetActive(false);
+    }
+
+    public void BackToMain()
+    {
+        if (MainMenuUI != null)
+            MainMenuUI.SetActive(true);
+    }
+
+    public void CloseLevelSelect()
+    {
+        if (levelSelectUI != null)
+            levelSelectUI.SetActive(false);
+    }
+
+    public void ExitGame()
     {
         Application.Quit();
     }
