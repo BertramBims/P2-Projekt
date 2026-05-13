3c2cfeda2cd2161f880d6381c6fabfd7c2885618
Bertram Winter <bertram.winter004@gmail.com>
2026-05-13 10:02:24 +0200

    big chaingeas wwagwaan

--- Assets/Scripts/Sceneloader.cs
+++ Assets/Scripts/Sceneloader.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class Sceneloader : MonoBehaviour
 {
     public string sceneToLoad;
+    [SerializeField] private bool loadOnCollision;
 
     public void LoadSpecificScene()
     {
@@ -19,5 +20,16 @@ public class Sceneloader : MonoBehaviour
     {
         int currentIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentIndex + 1);
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(loadOnCollision)
+        {
+            if (collision.CompareTag("Player"))
+            {
+                LoadSpecificScene();
+            }
+        }
     }
 }
