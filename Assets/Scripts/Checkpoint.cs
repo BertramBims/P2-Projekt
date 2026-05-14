ee24b776c45946bdf3f580a738c9018cb1043965
Bertram Winter <bertram.winter004@gmail.com>
2026-05-14 12:16:19 +0200

    Checkpoints & Dog Soundwave

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -2,15 +2,12 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-
-
     private void OnTriggerEnter2D(Collider2D other)
-{
-    if (other.CompareTag("Blind"))
     {
-
-        PlayerCheckpoint player = other.GetComponent<PlayerCheckpoint>();
-        player.SetCheckpoint(transform.position);
+        if (other.CompareTag("Player") || other.CompareTag("Mobility_Player"))
+        {
+            PlayerCheckpoint player = other.GetComponent<PlayerCheckpoint>();
+            player.SetCheckpoint(transform.position);
+        }
     }
-}
 }
